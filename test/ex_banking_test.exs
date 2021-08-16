c521e0dbc0200467e6afed2f785933b31c91b20d
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-08-15 22:53:46 -0400

    Added tests for withdrawing money

--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -40,4 +40,43 @@ defmodule ExBankingTest do
     assert ExBanking.deposit(@user, "23", "EUR") == {:error, :wrong_arguments}
     assert ExBanking.deposit(@user, -23, "EUR") == {:error, :wrong_arguments}
   end
+
+  test "rejects withdrawing money when the values are invalid" do
+    ExBanking.create_user(@user)
+    assert ExBanking.withdraw(@user, 23, "") == {:error, :wrong_arguments}
+    assert ExBanking.withdraw(@user, "23", "EUR") == {:error, :wrong_arguments}
+    assert ExBanking.withdraw(@user, -23, "EUR") == {:error, :wrong_arguments}
+  end
+
+  test "rejects withdrawing money of user that doesn't exist when there are existing users in the bank" do
+    ExBanking.create_user("george")
+    ExBanking.create_user("dimitri")
+    assert ExBanking.withdraw(@user, 23, "EUR") == {:error, :user_does_not_exist}
+  end
+
+  test "rejects withdrawing money of user when there are no users in the bank" do
+    assert ExBanking.withdraw(@user, 23, "EUR") == {:error, :user_does_not_exist}
+  end
+
+  test "rejects withdrawing more money than what the user has in the account of the given currency" do
+    ExBanking.create_user(@user)
+    ExBanking.deposit(@user, 100, "EUR")
+
+    assert ExBanking.withdraw(@user, 200, "EUR") == {:error, :not_enough_money}
+  end
+
+  test "rejects withdrawing money in a currency that doesn't have enough $$$ although the user have enough in other currencies" do
+    ExBanking.create_user(@user)
+    ExBanking.deposit(@user, 500, "EUR")
+    ExBanking.deposit(@user, 100, "USD")
+
+    assert ExBanking.withdraw(@user, 200, "USD") == {:error, :not_enough_money}
+  end
+
+  test "successfully withdraws money from an account in a given currency" do
+    ExBanking.create_user(@user)
+    ExBanking.deposit(@user, 500, "EUR")
+
+    assert ExBanking.withdraw(@user, 200, "EUR") == {:ok, 300}
+  end
 end
