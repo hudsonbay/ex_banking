2f11f9c831740741a1d3f7bd95212e71be7490cb
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-08-15 22:14:44 -0400

    Created find_user/2 validation

--- lib/ex_banking/validations.ex
+++ lib/ex_banking/validations.ex
@@ -6,4 +6,9 @@ defmodule ExBanking.Validations do
   def user_valid?(user) do
     !is_binary(user) or String.length(user) == 0
   end
+
+  def find_user(user_list, user) do
+    user_list
+    |> Enum.find(fn x -> Map.has_key?(x, user) end)
+  end
 end
