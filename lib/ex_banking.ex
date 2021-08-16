eb931cd90929da52308b71e3d6609cb06d90ccec
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-08-15 22:29:06 -0400

    Aliased module

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,5 +1,7 @@
 defmodule ExBanking do
   use GenServer
+
+  alias ExBanking.BankingUtils
   alias ExBanking.Validations
 
   # Client
@@ -23,6 +25,19 @@ defmodule ExBanking do
     end
   end
 
+  @doc """
+  - Increases user’s balance in given currency by amount value
+  - Returns new_balance of the user in given format
+  """
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def deposit(user, amount, currency) do
+    start_link()
+
+    GenServer.call(__MODULE__, {:deposit, user, amount, currency})
+  end
+
   # Server (callbacks)
 
   def init(_) do
@@ -37,6 +52,23 @@ defmodule ExBanking do
 
       _ ->
         {:reply, {:error, :user_already_exists}, user_list}
+    end
+  end
+
+  def handle_call({:deposit, user, amount, currency}, _from, user_list) do
+    case Validations.find_user(user_list, user) do
+      nil ->
+        {:reply, {:error, :user_does_not_exist}, user_list}
+
+      found_user ->
+        if Validations.valid_amount?(amount, currency) do
+          {new_amount, new_bank_state} =
+            BankingUtils.make_deposit(user, found_user, amount, currency, user_list)
+
+          {:reply, {:ok, new_amount}, new_bank_state}
+        else
+          {:reply, {:error, :wrong_arguments}, user_list}
+        end
     end
   end
 end
