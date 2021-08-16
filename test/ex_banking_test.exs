b45d705d6d2ce53f3f60e07b4de6e7c5668db14e
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-08-15 22:21:04 -0400

    Added new tests for user creation

--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -7,4 +7,15 @@ defmodule ExBankingTest do
   test "creates a user succesfully" do
     assert ExBanking.create_user(@user) == :ok
   end
+
+  test "rejects creating a user with wrong arguments" do
+    assert ExBanking.create_user("") == {:error, :wrong_arguments}
+    assert ExBanking.create_user(1) == {:error, :wrong_arguments}
+  end
+
+  test "rejects creating an already existent user" do
+    ExBanking.create_user(@user)
+
+    assert ExBanking.create_user(@user) == {:error, :user_already_exists}
+  end
 end
