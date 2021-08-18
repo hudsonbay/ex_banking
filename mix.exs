e1c3210e4f200a401a5cb9d33a1a8872b0d6fbff
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-08-17 23:43:24 -0400

    Installed dep to test parallel streams

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule ExBanking.MixProject do
   defp deps do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:parallel_stream, "~> 1.0.5"}
     ]
   end
 end
